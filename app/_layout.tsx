072a77cd0b631e3797bef87d92b22c9a0fb027c9
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-16 19:33:06 +0530

    feat: Small example of file based routing in expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
 import "../global.css"
+import { Stack } from 'expo-router'
 
 export default function RootLayout() {
     return (
-        <View>
-            <Text className='text-9xl text-red-300'>Wassup fucker</Text>
-        </View>
+        <Stack>
+            <Stack.Screen
+                name='index'
+            />
+        </Stack>
     )
 }
