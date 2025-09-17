6d4646bc04772c8b76678f9190233066818d6f2d
iamsurjog <iamsurjog@gmail.com>
2025-09-17 11:16:17 +0530

    feat: set up routes for advertise and discover

---
+++ app/p2p/discover/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text, Platform } from 'react-native'
+export default function Screen() {
+    return (
+        <View>
+            <Text>Testing Discover</Text>
+        </View>
+    )
+}
