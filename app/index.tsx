6fb43177485a29c5f1a586cddf66f4e6e50df068
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-17 10:55:12 +0530

    feat: Created Screen to test P2P Broadcasting

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,9 @@ import { View, Text, Button } from 'react-native'
 export default function index() {
     return (
         <View>
-            <Text>hello there</Text>
+            <Text className='text-4xl font-bold'>Welcome To BusBeacon</Text>
             <Button title='Go to Dashboard' onPress={() => router.push("/dashboard")} />
+            <Button title='Test P2P Broadcasting' onPress={() => router.push("/p2p")} />
         </View>
     )
 }
---
+++ app/p2p/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Screen() {
+    return (
+        <View>
+            <Text>Testing P2P Broadcasting</Text>
+        </View>
+    )
+}
